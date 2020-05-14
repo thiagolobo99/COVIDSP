36a8d07d69e8770ed2cc7de7f062e52a9aa656f6
Fernanda Nunes <fernanda.nunes@ibm.com>
2020-05-14 12:33:33 -0300

    add config to delivery pipeline

---
+++ src/app/main-dashboard/main-dashboard.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { CovidApiService } from 'src/services/covid-api.service';
+
+@Component({
+  selector: 'app-main-dashboard',
+  templateUrl: './main-dashboard.component.html',
+  styleUrls: ['./main-dashboard.component.scss']
+})
+export class MainDashboardComponent implements OnInit {
+
+  numberCases: any
+  numberDeaths: any
+
+
+  constructor(private covidApi: CovidApiService) { }
+
+  ngOnInit() {
+    this.covidApi.dataCovid().subscribe((response: any) => {
+      this.numberCases = response.Casos
+      this.numberDeaths = response.Mortes
+    })
+  }
+
+}
