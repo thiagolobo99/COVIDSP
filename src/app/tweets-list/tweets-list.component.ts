e289ba13c597ac26973ea464f05df6bbfdf227ae
Fernanda Nunes <fernanda.nunes@ibm.com>
2020-05-22 17:25:24 -0300

    adding error message when not does not find twitters

--- src/app/tweets-list/tweets-list.component.ts
+++ src/app/tweets-list/tweets-list.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { FezinhaService } from 'src/services/fezinha.service';
+import { Component, OnInit } from "@angular/core";
+import { FezinhaService } from "src/services/fezinha.service";
 
 @Component({
-  selector: 'app-tweets-list',
-  templateUrl: './tweets-list.component.html',
-  styleUrls: ['./tweets-list.component.scss']
+  selector: "app-tweets-list",
+  templateUrl: "./tweets-list.component.html",
+  styleUrls: ["./tweets-list.component.scss"],
 })
 export class TweetsListComponent implements OnInit {
-
-  constructor(private twitterService: FezinhaService) { }
+  constructor(private twitterService: FezinhaService) {}
 
   listTitle: string;
   arrayListTweets = [];
-  arrayApis = ["getCoronaVirusSp", "getQuarentenaSp", "getCoronaSantos", "getQuarentenaSantos", "getCoronaCampinas"];
+  arrayApis = [
+    "getCoronaVirusSp",
+    "getQuarentenaSp",
+    "getCoronaSantos",
+    "getQuarentenaSantos",
+    "getCoronaCampinas",
+  ];
 
   ngOnInit() {
     this.returnTwitter("getCoronaVirusSp");
+
     this.receiveInformationFromApi(this.arrayApis);
   }
 
@@ -27,42 +33,39 @@ export class TweetsListComponent implements OnInit {
           this.receiveInformationFromApi(this.arrayApis);
         }
       }, 30000 * arrayApis.length - 30000 * position);
-
-    })
+    });
   }
 
   returnTwitter(objTwitter) {
     this.twitterService.listTwitter(objTwitter).subscribe((response: any) => {
       this.arrayListTweets = [];
 
+      if (this.arrayListTweets.length == 0) {
+      }
       this.listTitle = objTwitter;
       if (this.listTitle == "getQuarentenaSantos")
-        this.listTitle = "Quarentena em Santos"
+        this.listTitle = "Quarentena em Santos";
       if (this.listTitle == "getQuarentenaCampinas")
-        this.listTitle = "Quarentena em Campinas"
+        this.listTitle = "Quarentena em Campinas";
       if (this.listTitle == "getQuarentenaSp")
-        this.listTitle = "Quarentena em São Paulo"
+        this.listTitle = "Quarentena em São Paulo";
 
       if (this.listTitle == "getCoronaVirusSp")
-        this.listTitle = "Coronavírus em São Paulo"
+        this.listTitle = "Coronavírus em São Paulo";
       if (this.listTitle == "getCoronaCampinas")
-        this.listTitle = "Coronavírus em Campinas"
+        this.listTitle = "Coronavírus em Campinas";
       if (this.listTitle == "getCoronaSantos")
-        this.listTitle = "Coronavírus em Santos"
+        this.listTitle = "Coronavírus em Santos";
 
       response.result.map((respostas) => {
-
-
-        this.arrayListTweets.push(
-          {
-            text: respostas.tweet,
-            subject: "#" + respostas.sentiment[0].text,
-            label: respostas.sentiment[0].label
-          }
-        );
+        this.arrayListTweets.push({
+          text: respostas.tweet,
+          subject: "#" + respostas.sentiment[0].text,
+          label: respostas.sentiment[0].label,
+        });
       });
     });
-    console.log(this.arrayListTweets);
-  }
 
+    // console.log("ok", this.arrayListTweets);
+  }
 }
